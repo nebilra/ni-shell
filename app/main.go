0699720d5ed146ffae886e26d9dfa70d04e5406f
Nebil <a0911991679@gmail.com>
2026-08-22 10:04:01 +0300

    handle exit builtin

--- app/main.go
+++ app/main.go
@@ -11,12 +11,17 @@ func main() {
 	for {
 		fmt.Print("$ ")
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		command = strings.TrimSuffix(command, "\n")
 
 		if err != nil {
 			panic(err)
 		}
+		switch command {
+		case "exit":
+			os.Exit(0)
+		}
 
-		fmt.Printf("%s: command not found\n", strings.TrimSuffix(command, "\n"))
+		fmt.Printf("%s: command not found\n", command)
 	}
 
 }
