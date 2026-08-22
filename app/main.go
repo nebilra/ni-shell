57ee6ec025e2462ee956d4dc2b29e2208f6c51bc
Nebil <a0911991679@gmail.com>
2026-08-22 10:14:52 +0300

    handle echo builtin

--- app/main.go
+++ app/main.go
@@ -11,17 +11,21 @@ func main() {
 	for {
 		fmt.Print("$ ")
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		command = strings.TrimSuffix(command, "\n")
+		command = strings.TrimSpace(command)
+		cmd := strings.Split(command, " ")
 
 		if err != nil {
 			panic(err)
 		}
-		switch command {
+		switch cmd[0] {
 		case "exit":
 			os.Exit(0)
+		case "echo":
+			fmt.Println(strings.Join(cmd[1:], " "))
+			continue
+		default:
+			fmt.Printf("%s: command not found\n", command)
 		}
-
-		fmt.Printf("%s: command not found\n", command)
 	}
 
 }
