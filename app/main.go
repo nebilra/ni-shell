c05b4c67afd92386794ff67e6bceb8e76d4d7a57
Nebil <a0911991679@gmail.com>
2026-08-31 16:44:20 +0300

    list matches on second tab; reprompt

--- app/main.go
+++ app/main.go
@@ -242,15 +242,16 @@ func (ac *AutoCompleter) Do(line []rune, pos int) (newLine [][]rune, length int)
 			return
 		}
 		ac.tabbed = false
-		// var options strings.Builder
-		// options.WriteString("\n")
-		// for _, val := range out {
-		// 	options.WriteString(string(line))
-		// 	options.WriteString(string(val))
-		// 	options.WriteString(" ")
-		// }
-		// fmt.Println(options.String())
-		// return out, 0
+		var options strings.Builder
+		options.WriteString("\n")
+		for _, val := range out {
+			options.WriteString(string(line))
+			options.WriteString(string(val))
+			options.WriteString(" ")
+		}
+		fmt.Println(options.String())
+		fmt.Printf("$ %s", string(line))
+		return nil, 0
 	}
 
 	return out, pos
